0184213e1dc2c910c3f538a6b069126e7ce56338
Scott Duke <scottdookey@gmail.com>
2019-03-09 17:15:49 +0000

    added test for adding artist

--- spec/lib/tasks/import_rake_spec.rb
+++ spec/lib/tasks/import_rake_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require 'rails_helper'
 require 'rake'
 
 describe 'import rake task' do
@@ -8,17 +8,41 @@ describe 'import rake task' do
   end
 
   describe 'import:video_data' do
-    before do
-      allow(ImportHelper).to receive(:get_video_data).and_return("")
+    let :example_data do
+      [
+        {
+          "video_uid": "wWRIyRRyTCI", 
+          "song": {
+            "id": 3976, "artist_id": 2745, "title": "Brunch", "cached_slug": "brunch", "city_id": 58, 
+            "artist": {
+              "id": 2745, "title": "Xoe Wise", "cached_slug": "xoe-wise"
+            }, 
+            "city": {
+              "id": 58, "title": "Chicago", "cached_slug": "chicago"
+            }
+          }
+        }
+      ].to_json
     end
+
+    let(:parsed_data) { JSON.load(example_data) }
 
     let :run_rake_task do
       Rake::Task["import:video_data"].reenable
       Rake.application.invoke_task "import:video_data"
     end
 
+    before do
+      allow(ImportHelper).to receive(:get_video_data).and_return(parsed_data)
+    end
+
     it "calls ImportHelper.get_video_data with video data url" do
       expect(ImportHelper).to receive(:get_video_data).with("https://s3-eu-west-1.amazonaws.com/sofar-eu-1/video_data.json")
+      run_rake_task
+    end
+
+    it "creates new or find new artist" do
+      expect(Artist).to receive(:find_or_create_by).with(parsed_data.first["song"]["artist"]).and_return(nil)
       run_rake_task
     end
   end
